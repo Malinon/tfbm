8c9eb18a0bc764068e50d9e3b6e9bf55648c185e
Jakub <jakubbmalinowski@gmail.com>
2024-10-19 19:37:23 +0200

    Add package structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tfbm',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='Package for simulating tempered fractional Brownian motion',
+    author='Jakub Malinowski',
+    author_email='jakub.malinowski@pwr.edu.pl',
+    url='https://github.com/Malinon/tfbm',
+    license='MIT',
+)
