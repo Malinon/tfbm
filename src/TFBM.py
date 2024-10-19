d37cd7986d3f82c30f04dbc865af4184f49cffde
Jakub <jakubbmalinowski@gmail.com>
2024-10-19 09:20:26 +0200

    Corrections in generators

--- src/TFBM.py
+++ src/TFBM.py
@@ -29,14 +29,14 @@ class TFBM:
 
     def covariance_matrix(self):
         """ Generates covariance matrix of TFBM process """
-        sigma = np.zeros((len(self.ts), len(self.ts)))
+        sigma = np.zeros((len(self.ts), len(self.ts))) # Covariance matrix buffer
         ct_2_values = [float(self.ct_2(t)) for t in self.ts]
         for t in range(len(self.ts)):
             for s in range(len(self.ts)):
                 sigma[t,s] = (ct_2_values[t] + ct_2_values[s] - ct_2_values[abs(t-s)]) / 2
         return sigma
     
-    def generate_and_save_cov(self):
+    def _generate_and_save_cov(self):
         """ Generates and saves covariance matrix of TFBM process """
         sigma = self.covariance_matrix()
         h_str = str(self.H).replace('.', '_')
@@ -44,7 +44,7 @@ class TFBM:
         np.savetxt(self.cov_matrices_dir + f"/H_{h_str}_l_{l_str}_T_{str(self.T)}_N_{str(self.N)}.txt", sigma)
         return sigma
 
-    def load_cov_matrix(self):
+    def _load_cov_matrix(self):
         """ Loads covariance matrix of TFBM process from file """
         h_str = str(self.H).replace('.', '_')
         l_str = str(self.lambd).replace('.', '_')
@@ -52,62 +52,75 @@ class TFBM:
         if os.path.isfile(filename):
             sigma = np.loadtxt(filename)
         else:
-            sigma = self.generate_and_save_cov()
+            sigma = self._generate_and_save_cov()
         return sigma
     
     def cholesky_decomp_from_file(self):
         """ Loads covariance matrix from file and returns its Cholesky decomposition """
-        sigma = self.load_cov_matrix()
+        sigma = self._load_cov_matrix()
+        # It looks ok, but discuss with advisor about this fragment
         sigma = np.delete(sigma, 0, 0)
         sigma = np.delete(sigma, 0, 1)
         return np.linalg.cholesky(sigma)
 
-    def generate_dh_increments(self, eigenvals):
+    def _generate_dh_increments(self, eigenvals):
         Z_even = np.random.normal(0, 1, self.n - 1)
         Z_odd = np.random.normal(0, 1, self.n - 1)
+
         Y = np.zeros(2*self.n, dtype=complex)
-        Y[1:int(self.n)] = Z_odd + 1j * Z_even
-        Y[int(self.n)+1:] = np.conj(Y[1:int(self.n)][::-1])
-        Y[0] = np.sqrt(2) * np.random.normal(0, 1) / np.sqrt(2) 
-        Y[int(self.n)] =  np.random.normal(0, 1) / np.sqrt(2)  
+        Y[self.n] = Z_odd + 1j * Z_even
+        Y[self.n + 1:] = np.conj(Y[1:self.n][::-1])
+        Y[0] = np.random.normal(0, 1) / np.sqrt(2) 
+        Y[self.n] =  np.random.normal(0, 1) / np.sqrt(2)
+        
         Y = np.sqrt(eigenvals) * Y
 
-        X = np.real(1/np.sqrt(self.n * 2) * np.fft.fft(Y))[:self.n]
+        X = np.real(np.fft.fft(Y) / np.sqrt(self.n * 2))[:self.n]
         return X
     
     def _generate_row_of_cirular_matrix(self):
+        """ Generates row of circulant matrix which is used in Davies-Harte method """
         # Cache Ct_2 values
         ct_2_values = [self.ct_2(t) for t in self.ts] + [self.ct_2(self.ts_[-1] + self.dt)]
-        row = np.zeros(2 * (self.N -1))
-        for k in range(1, self.N-1):
-            row[k] = 0.5 * (ct_2_values[k + 1] - (2 * ct_2_values[k]) + ct_2_values[abs(k-1)])
+        autocovariance = lambda k: 0.5 * (ct_2_values[k + 1] - (2 * ct_2_values[k]) + ct_2_values[abs(k - 1)])
+
+        row = np.zeros(2 * self.n)
+        for k in range(1, self.n):
+            row[k] = autocovariance(k)
             row[-k] = row[k]
-        row[self.N -1] = 0.5 * (ct_2_values[self.N] - (2 * ct_2_values[self.N-1]) + ct_2_values[abs(self.N-2)])
-        row[0] = 0.5 * (ct_2_values[1] - (2 * ct_2_values[0]) + ct_2_values[1])
-        row[-1] = row[1]
-        return gammas
+        row[self.n] = autocovariance(self.n)
+        row[0] = autocovariance(0)
+    
+        return row
     
     def generate_samples(self, num_of_samples, get_increments=False):
         if self.method == "davies-harte":
+            # Use Davies-Harte method to generate Tempered Fractional Gaussian Noise and transform it to TFBM
+
             row = self._generate_row_of_cirular_matrix()
             # Find eigenvalues of circulant matrix
             eigenvals = np.fft.fft(row).real
+
             if not all(si > 0 for si in eigenvals):
                 print(f"H: {self.H}, lambda: {self.lambd}")
                 print("Switching to cholesky method")
                 self.method = "cholesky"
                 samples = self.generate_samples(num_of_samples, get_increments)
             else:
-                samples = list(np.zeros(num_of_samples))
-                increments = list(np.zeros(num_of_samples))
+                samples = []
+                increments = []
                 for i in range(num_of_samples):
-                    incr = self.generate_dh_increments(eigenvals)
-                    samples[i] = np.insert(np.cumsum(incr), 0, [0], axis=None)
-                    increments[i] = incr
+                    incr = self._generate_dh_increments(eigenvals)
+                    samples.append(np.insert(np.cumsum(incr), 0, [0], axis=None))
+                    increments.append(incr)
         else:
-            ts = self.ts[1:]
-            Z = np.random.normal(size=(len(ts), num_of_samples)) # Generate an (n + 1) column vector of i.i.d. standard normal r.v.
-            L = self.cholesky_decomp_from_file() 
+            # Generate samples using Cholesky method
+            
+            Z = np.random.normal(size=(self.n, num_of_samples)) # Generate an (n + 1) column vector of i.i.d. standard normal r.v.
+            L = self.cholesky_decomp_from_file()
+            
+            # Perform sum 4.4 from Asmussen (It works, because L is lower triangular matrix)
+            # and insert 0 at the beginning of each trajectory
             samples = np.insert(np.matmul(L,Z), 0, [0], axis=0)
             increments = np.diff(samples, axis=0)
         if get_increments:
