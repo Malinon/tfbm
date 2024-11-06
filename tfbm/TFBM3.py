6d00977b38ed2e767b410cc4a94e68f4e0ffb2cc
Jakub <jakubbmalinowski@gmail.com>
2024-11-06 22:55:34 +0100

    TFBM3 correction

--- tfbm/TFBM3.py
+++ tfbm/TFBM3.py
@@ -1,22 +1,20 @@
 from .TFBM2 import TFBM
-from scipy.special import gamma
+from scipy.special import gamma, factorial
 
-def mittag_leffler(alpha, beta, gamm, z, tolerance=1e-20):
+def mittag_leffler(alpha, beta, gamm, z, tolerance=1e-20, max_iter=100):
     prev_sum = 0.0
     k = 1
-    factorial_k = 1
-    result = (gamma(gamm)) / (factorial_k * gamma(beta))
-    while abs(result - prev_sum) > tolerance:
+    result = (gamma(gamm)) / (gamma(beta))
+    while abs(result - prev_sum) > tolerance and k < max_iter:
         prev_sum = result
-        term = (gamma(gamm + k) * z**k) / (factorial_k * gamma(alpha * k + beta))
+        term = (gamma(gamm + k) * z**k) / (factorial(k) * gamma(alpha * k + beta))
         k += 1
-        factorial_k *= k
         result += term
     return result / gamma(gamm)
 
 class TFBM3(TFBM):
     def __init__(self, T, N, H, lambd, method="davies-harte"):
-        super().__init__(T, N, H, lambd, gamma_H, method)
+        super().__init__(T, N, H, lambd, method)
         self.cov_matrices_dir = "cov_matrices_tfbm3"
         self._gamm = 1 - 2 * self.H
         self._alpha = 1
@@ -25,7 +23,7 @@ class TFBM3(TFBM):
         self.cov_matrices_dir = "cov_matrices_tfbm3"
 
     def ct_2(self, t):
-        # Assumption k_B  * T / (m * gamma_H) = 1 
+        # Assumption k_B  * T / (m * gamma_H) = 1
         return 2 *  t**(self._exponent) * mittag_leffler(self._alpha,
                                                          self._beta,
                                                          self._gamm, -t / self.lambd)
