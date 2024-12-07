fb7c5264a7ba3ffa210e0de88f85f963a2a82908
Jakub <jakubbmalinowski@gmail.com>
2024-12-07 13:51:40 +0100

    Adapt for multiprocessing

--- utils/cluster_generation.py
+++ utils/cluster_generation.py
@@ -3,6 +3,7 @@ import tfbm
 import numpy as np
 import sys
 import json
+from multiprocessing import Pool
 
 T = 1
 
@@ -10,6 +11,7 @@ TRAJECTORIES_PER_PARAMETERS = int(sys.argv[1])
 TFBM_TYPE = int(sys.argv[3])
 OUTPUT_DIRETORY = sys.argv[2]
 PARAMETERS_CONFIG_PATH = sys.argv[4]
+WORKERS = int(sys.argv[5])
 
 with open(PARAMETERS_CONFIG_PATH) as json_file:
     parameters = json.load(json_file)
@@ -38,6 +40,11 @@ def generate_trajectories_for_parameter(H, lambd, n_trajectories):
 
     np.save(f'{OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd}.npy', trajectories)
 
-for H in np.linspace(MIN_H, MAX_H, N_H):
-    for lambd in np.linspace(MIN_LAMBDA, MAX_LAMBDA, N_LAMBDA):
-        generate_trajectories_for_parameter(H, lambd, TRAJECTORIES_PER_PARAMETERS)
+def worker(params):
+    H, lambd = params
+    generate_trajectories_for_parameter(H, lambd, TRAJECTORIES_PER_PARAMETERS)
+
+if __name__ == "__main__":
+    params_list = [(H, lambd) for H in np.linspace(MIN_H, MAX_H, N_H) for lambd in np.linspace(MIN_LAMBDA, MAX_LAMBDA, N_LAMBDA)]
+    with Pool(WORKERS) as pool:
+        pool.map(worker, params_list)
