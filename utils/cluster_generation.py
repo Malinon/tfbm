6beaf406820a0d18f7d88bc4cfd0fa65e27814aa
Jakub Malinowski <jakmal7178@ui.wcss.pl>
2025-01-02 10:49:13 +0100

    Final generation

--- utils/cluster_generation.py
+++ utils/cluster_generation.py
@@ -2,6 +2,7 @@ import tfbm
 
 import numpy as np
 import sys
+import os.path
 import json
 from multiprocessing import Pool
 
@@ -31,18 +32,26 @@ elif TFBM_TYPE == 3:
     TFBM = tfbm.TFBM3
 
 def generate_trajectories_for_parameter(H, lambd, n_trajectories):
+    print(f'Started {OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd}.npy')
     generator = TFBM(N=1000, H=H, lambd=lambd, T=T)
     try:
         trajectories =  generator.generate_samples(n_trajectories)
     except np.linalg.LinAlgError as e:
         print("LinAlgError for H: {H} and lambda: {lambd}")
         print(f"Error: {e}")
-
+        return
+    except Exception as e:
+        print("Unkown exception :", e)
+        print(f'{OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd} FAILED')
+        return
     np.save(f'{OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd}.npy', trajectories)
+    print(f'Done {OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd}.npy')
+    sys.stdout.flush()
 
 def worker(params):
     H, lambd = params
-    generate_trajectories_for_parameter(H, lambd, TRAJECTORIES_PER_PARAMETERS)
+    if not os.path.exists(f'{OUTPUT_DIRETORY}/TYPE_{TFBM_TYPE}_T_{T}_H_{H}_lambda_{lambd}.npy'):
+        generate_trajectories_for_parameter(H, lambd, TRAJECTORIES_PER_PARAMETERS)
 
 if __name__ == "__main__":
     params_list = [(H, lambd) for H in np.linspace(MIN_H, MAX_H, N_H) for lambd in np.linspace(MIN_LAMBDA, MAX_LAMBDA, N_LAMBDA)]
