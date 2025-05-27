5ddc79ae4dd1b4f945ee3986d08bfe6631382f5e
Jakub <jakubbmalinowski@gmail.com>
2025-05-27 16:34:41 +0200

    Add simplified Mittag-Leffer

--- tfbm/TFBM3.py
+++ tfbm/TFBM3.py
@@ -21,6 +21,27 @@ def mittag_leffler(alpha, beta, gamm, z, tolerance=1e-20):
 
     return result / gamma(gamm)
 
+# We only need M-L function for alpha=1, beta=3-2*H, gamm=1-2*H
+def simplified_mittag_leffler(H, z, tolerance=1e-20):
+    prev_sum = 0.0
+    k = 1
+    gamm = 1 - 2 * H
+    beta = 3 - 2 * H
+    if H == 0.5:
+        # Extreme case H = 0.5
+        return 1.0 / gamma(gamm)
+    result = (gamma(gamm)) / (gamma(beta))
+    while abs(result - prev_sum) > tolerance:
+        prev_sum = result
+        term = ((z**k) / (factorial(k))) / ((k + 2 - 2 * H) * (k + gamm)) 
+        k += 1
+        if term != np.inf and term in [np.nan] and term != -np.inf:
+            result += term
+        else:
+            break
+
+    return result / gamma(gamm)
+
 class TFBM3(TFBM):
     def __init__(self, T, N, H, lambd, method="davies-harte"):
         super().__init__(T, N, H, lambd, method)
@@ -33,8 +54,6 @@ class TFBM3(TFBM):
 
     def ct_2(self, t):
         # Assumption k_B  * T / (m * gamma_H) = 1
-        return 2 *  t**(self._exponent) * mittag_leffler(self._alpha,
-                                                         self._beta,
-                                                         self._gamm, -t / self.lambd)
+        return 2 *  t**(self._exponent) * simplified_mittag_leffler(self.H, -t / self.lambd)
 
     
