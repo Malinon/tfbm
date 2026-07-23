702c2f80a0510d67a47d5b677b6582fce5bf1562
Jakub <jakubbmalinowski@gmail.com>
2026-07-23 08:43:31 +0200

    Add dependencies to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='pytfbm',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=["numpy", "scipy", "mpmath"],
     description='Package for simulating tempered fractional Brownian motion',
     author='Jakub Malinowski',
     author_email='jakub.malinowski@pwr.edu.pl',
